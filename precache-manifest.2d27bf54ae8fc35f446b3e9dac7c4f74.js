686c779151372a29ff7f45f502ef5df4eec82b32
watcharapong.c <ne7shii@gmail.com>
2020-06-17 16:32:18 +0700

    Updates

---
+++ precache-manifest.2d27bf54ae8fc35f446b3e9dac7c4f74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d656e913360e06f38044e323e5bf8587",
+    "url": "/react-layout/index.html"
+  },
+  {
+    "revision": "59ddd4d165b9df50bf32",
+    "url": "/react-layout/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b53b18401a40806c7112",
+    "url": "/react-layout/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "59ddd4d165b9df50bf32",
+    "url": "/react-layout/static/js/2.c283fff7.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-layout/static/js/2.c283fff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b53b18401a40806c7112",
+    "url": "/react-layout/static/js/main.620d67c6.chunk.js"
+  },
+  {
+    "revision": "7a3bda133b9abdc4788f",
+    "url": "/react-layout/static/js/runtime-main.116fd68f.js"
+  }
+]);
